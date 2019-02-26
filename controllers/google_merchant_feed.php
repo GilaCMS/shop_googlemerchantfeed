81051c974c0e9b2316be09ab9cc3a83c6147e6c5
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-02-26 06:38:07 -0600

    description clean

--- controllers/google_merchant_feed.php
+++ controllers/google_merchant_feed.php
@@ -12,15 +12,15 @@ class google_merchant_feed extends controller
   function indexAction ()
   {
     global $db;
-    //$products = product::get([], 10000);
-
+    $currency = gila::option('shop.currency');
     $products = $db->gen("SELECT sp.id,sp.title,sp.upc,sp.image,sp.description,sp.price,(CASE WHEN new_price='' THEN sp.price ELSE new_price END) as sale_price,sku.stock as stock FROM shop_product sp, shop_sku sku WHERE product_id=sp.id AND sku.stock>0;");
 
-    echo "id\ttitle\tprice\tdescription\tlink\timage_link\tavailability\tsale_price\n";
+    echo "id\ttitle\tprice\tdescription\tlink\timage_link\tavailability\tsale_price\r\n";
     foreach($products as $p) {
-      echo $p[0]."\t".$p[1]."\t".$p['price']."\t".$p['description']."\t";
-      echo gila::url('shop/product/'.$p[0])."\t";
-      echo $p['image']."\tin stock\t".$p['sale_price']."\n";
+      echo $p[0]."\t".$p[1]."\t".$p['price']." $currency\t";
+      echo "'".str_replace(["\t","'","\r","\n"], "", $p['description'])."'\t";
+      echo gila::base_url('shop/product/'.$p[0])."\t";
+      echo gila::config('base').$p['image']."\tin stock\t".$p['sale_price']." $currency\r\n";
     }
   }
 }
